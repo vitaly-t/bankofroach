b4231afe901cdb22b1892f7999c87b21ef6614fd
Sean Loiselle <sean@cockroachlabs.com>
2017-04-25 21:10:54 -0400

    cleaning up dependencies

--- node_modules/generator-express/app/templates/extras/mvc-coffee/thinky-config.coffee
+++
@@ -1,5 +0,0 @@
-config = require('./config')
-
-thinky = require('thinky')(config.db)
-
-module.exports = thinky
--- node_modules/generator-express/app/templates/extras/mvc/thinky-config.js
+++
@@ -1,5 +0,0 @@
-var config = require('./config');
-
-var thinky = require('thinky')(config.db);
-
-module.exports = thinky;
